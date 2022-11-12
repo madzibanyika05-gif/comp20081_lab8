5a39309a1f2972b5274a98ada30a2dc8153c67f4
Your Name <you@example.com>
2022-11-12 00:42:59 +0000

    update

--- lab8/src/main/java/com/mycompany/lab8/Ex1.java
+++ lab8/src/main/java/com/mycompany/lab8/Ex1.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.lab9_10;
+package com.mycompany.lab8;
 
 import java.util.*;
 
